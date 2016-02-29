13fe702550f78c4ccb035abaa2e1f7b468d4a8f6
Martin Cross <mncross@gmail.com>
2016-02-28 20:45:45 -0600

    setup solution

---
+++ Calc/UnitTestProject1/CalcTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void ExpressionTest()
+//Prove you can extract the terms of the expression.
+//Prove you can extract the operation embedded in the expression.
+//Ensure you have examples of GOOD and BAD input and have you Parse class throw an exception when there's an error.
+
+//Your expression should work with and without spaces. (i.e.both 2+1 and 2 + 1 should work)
+//The use of Regular Expressions is not required.
+//GOOD input example: 1+2
+//BAD input example +2
+//You do not need to handle the cases of usage of parenthesis or expressions with more than 2 terms.
+        {
+            
+        }
+    }
+}
