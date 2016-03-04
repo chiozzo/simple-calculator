68c8091f05dd4806b5697b6faf291b45e0ce1f5a
Martin Cross <mncross@gmail.com>
2016-03-04 11:32:01 -0600

    add error handling for parsing

--- Calc/Calc/ParseExpression.cs
+++ Calc/Calc/ParseExpression.cs
@@ -15,10 +15,36 @@ namespace Calc
         {
             string userExpressionString = userExpression.Replace(" ", "");
             int operandIndex = userExpressionString.IndexOfAny(new char[] { '+', '-', '*', '/', '%' });
+            if (operandIndex == -1)
+            {
+                throw new ArgumentException("You didn't provide a valid operator.");
+            }
             char operand = userExpressionString[operandIndex];
             string[] terms = userExpression.Split(operand);
-            int term1 = Convert.ToInt32(terms[0]);
-            int term2 = Convert.ToInt32(terms[1]);
+            if (terms.Length != 2)
+            {
+                throw new ArgumentException("You didn't provide enough terms for me to calculate.");
+            }
+            int term1;
+            int term2;
+            try
+            {
+                term1 = Convert.ToInt32(terms[0]);
+            }
+            catch (Exception)
+            {
+
+                throw new ArgumentException("Your first term is not a valid integer.");
+            }
+            try
+            {
+                term2 = Convert.ToInt32(terms[1]);
+            }
+            catch (Exception)
+            {
+
+                throw new ArgumentException("Your second term is not a valid integer.");
+            }
             object[] parsedExpression = { term1, operand, term2 };
             return parsedExpression;
         }
