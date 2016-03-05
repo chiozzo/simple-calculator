6991c2b225e78e187bc003c28d5ec55922c54564
Martin Cross <mncross@gmail.com>
2016-03-05 13:36:59 -0600

    revisions to UI

---
+++ Calc/Calc/Terminal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Terminal
+    {
+        public int counter { get; set; }
+
+        public Terminal()
+        {
+            counter = 0;
+        }
+
+        public int IncrementExpressionCounter()
+        {
+            counter++;
+            return counter;
+        }
+
+        public string PromptForExpression()
+        {
+            Console.Write("[{0}]> ", counter);
+            string userExpressionString = Console.ReadLine();
+            return userExpressionString;
+        }
+
+        public void ReturnExpressionAnswer(double answer)
+        {
+            Console.WriteLine("   = {0}", answer);
+            counter++;
+        }
+    }
+}
