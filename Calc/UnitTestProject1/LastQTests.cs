5806f295ee89655e100e3e57db2e67662b6cb8de
Martin Cross <mncross@gmail.com>
2016-03-05 13:33:59 -0600

    begin work on retrieving last entered expression

---
+++ Calc/UnitTestProject1/LastQTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calc;
+
+namespace CalcTest
+{
+    [TestClass]
+    public class LastQTests
+    {
+        [TestMethod]
+        public void ProveRetrieveLastQ()
+        {
+            // Arrange
+            LastExpression lastQ= new LastExpression();
+            lastQ.lastQ = "5 + 7";
+            // Act
+            string actual = "5 + 7";
+            string expected = lastQ.lastQ;
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
